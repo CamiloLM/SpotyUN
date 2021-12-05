f003fd04101d882d83ef42dd0dc465d11069d619
Camilo Londoño Moreno <camilo.londonom@gmail.com>
2021-12-04 21:11:50 -0500

    Tablas Creadas

--- crud.py
+++ crud.py
@@ -3,25 +3,68 @@ import sqlite3
 def conexion():
     try:
         con = sqlite3.connect('SpotyUN.db')
+        con.execute("PRAGMA foreign_keys = 1")
         return con
     except sqlite3.Error:
         print(sqlite3.Error)
 
 
 def crear_tablas(con, cur):
-    cur.execute("CREATE TABLE IF NOT EXISTS cancion (ID integer PRIMARY KEY AUTOINCREMENT, Nombre text, Genero text, Album text, Interprete text)")
-    cur.execute("CREATE TABLE IF NOT EXISTS usuario (ID integer PRIMARY KEY AUTOINCREMENT, Cedula integer, Nombre text, Apellido text, Pais text, Ciudad text, Telefono integer, FechaPago text, TDD integer, Pago integer)")
+    cur.execute('''CREATE TABLE IF NOT EXISTS cancion (
+        codigo INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        urlCancion TEXT NOT NULL,
+        genero TEXT,
+        album TEXT,
+        interprete TEXT
+        )''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS cliente (
+        cedula INTEGER PRIMARY KEY,
+        nombre TEXT NOT NULL,
+        apellido TEXT NOT NULL,
+        correo TEXT NOT NULL,
+        pais TEXT,
+        ciudad TEXT,
+        telefono INTEGER,
+        fechaPago TEXT,
+        targetaCredito INTEGER,
+        pago INTEGER
+        )''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS planes (
+        codigo INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        valor float,
+        cantidad INTEGER
+        )''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS subscripciones (
+        codigoPlan INTEGER,
+        cedulaCliente INTEGER,
+        FOREIGN KEY (codigoPlan) REFERENCES planes (codigo),
+        FOREIGN KEY (cedulaCliente) REFERENCES cliente (cedula)
+        )''')
+    
+    cur.execute('''CREATE TABLE IF NOT EXISTS listaCanciones (
+        codigo INTEGER PRIMARY KEY AUTOINCREMENT,
+        codigoCancion INTEGER,
+        cedulaCliente INTEGER,
+        FOREIGN KEY (codigoCancion) REFERENCES cancion (codigo),
+        FOREIGN KEY (cedulaCliente) REFERENCES cliente (cedula)
+        )''')
+    
     con.commit()
 
 
-def insertar_tabla_cancion (con, cur, cancion):
-    cur.execute('''INSERT INTO cancion VALUES (?,?,?,?,?)''', cancion)
-    con.commit()
+# def insertar_tabla_cancion (con, cur, cancion):
+#     cur.execute('''INSERT INTO cancion VALUES (?,?,?,?,?)''', cancion)
+#     con.commit()
 
 
-def insertar_tabla_usuario (con, cur, usuario):
-    cur.execute('''INSERT INTO usuario VALUES (?,?,?,?,?,?,?,?,?)''', usuario)
-    con.commit()
+# def insertar_tabla_usuario (con, cur, usuario):
+#     cur.execute('''INSERT INTO usuario VALUES (?,?,?,?,?,?,?,?,?)''', usuario)
+#     con.commit()
 
 # Codigo aun no adaptado
 
@@ -58,4 +101,5 @@ def insertar_tabla_usuario (con, cur, usuario):
 if __name__ == "__main__":
     con = conexion()
     cur = con.cursor()
+    crear_tablas(con, cur)
     con.close()
