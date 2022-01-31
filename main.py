0b1a9863ef4a7da60578048cd8a370bc8a99f820
Alejandro Muñoz <bmunozp@unal.edu.co>
2022-01-30 22:50:17 -0500

    assets - canciones y fotos de almbums

--- main.py
+++ main.py
@@ -14,11 +14,13 @@ environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"  # Oculta mensaje de Pygame
 def conexion_base_datos():
     """Crea una conexión con la base de datos, si no existe se crea una vacia."""
     try:
-        conn = sqlite3.connect('SpotyUN.db')  # Retorna una conexón sqlite con la base de datos del programa.
+        # Retorna una conexón sqlite con la base de datos del programa.
+        conn = sqlite3.connect('SpotyUN.db')
         conn.execute("PRAGMA foreign_keys = 1")  # Activa la selectividad de las llaves foraneas.
         return conn
     except sqlite3.Error:
-        print(sqlite3.Error)  # En caso de que suceda un error grave el programa atrapa e imprime el error.
+        # En caso de que suceda un error grave el programa atrapa e imprime el error.
+        print(sqlite3.Error)
 
 
 if __name__ == "__main__":
@@ -27,7 +29,8 @@ if __name__ == "__main__":
     print("╚" + "═"*32 + "╝")
 
     conexion = conexion_base_datos()  # Almacena un objetos con la conexión a la base de datos.
-    cursor = conexion.cursor()  # Almacena un objeto cursor para realizar selecciones en la base da datos.
+    # Almacena un objeto cursor para realizar selecciones en la base da datos.
+    cursor = conexion.cursor()
 
     while True:
         print("\nSeleccione que opciones desea ver:")
@@ -61,9 +64,10 @@ if __name__ == "__main__":
             menu_subscripción(conexion, cursor)
 
         elif case == "7":
-            cursor.execute("SELECT name FROM sqlite_schema WHERE type='table'")  # Busca el nombre de todas las tablas.
+            # Busca el nombre de todas las tablas.
+            cursor.execute("SELECT name FROM sqlite_schema WHERE type='table'")
             tablas = cursor.fetchall()  # El nombre queda guardado en la vatiable tablas.
-            
+
             # Se ejecuta si hay tablas en la base de datos.
             if tablas:
                 for tabla, in tablas:  # Recorre la lista de tablas para borrarla una a una.
@@ -71,7 +75,7 @@ if __name__ == "__main__":
                         sql = f"DROP TABLE {tabla}"
                         cursor.execute(sql)  # Ejecuta el comando para borrar los datos de la tabla.
                 conexion.commit()  # Los cambios son llevados a cabo en la base de datos.
-            
+
             crear_tablas(conexion, cursor)  # Crea las tablas del la base de datos si no existe.
             print("La base de datos ha sido creada desde cero.")
 
