54c15979664fb8e28eca76811c7ad67771c44080
Alejandro Muñoz <bmunozp@unal.edu.co>
2022-02-07 11:13:47 -0500

    Correcciones GUI. No pude realizar el filtro.

--- planesUI.py
+++ planesUI.py
@@ -3,9 +3,11 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtSql import *
+from QTable import *
 from multiprocessing.spawn import prepare
 import sys
 """Importa las librerías de PyQT5 para su correcto funcionamiento"""
+
 
 def ConBD():
     """Conexión con la bd de SQLite por medio de PyQT"""
@@ -15,94 +17,95 @@ def ConBD():
         q = QSqlQuery()
         if (q.prepare("create table if no exists planes(codigo integer primary key autoincrement not null, nombre text not null, valor float not null, cantidad integer not null) ")):
             if (q.exec()):
-                print ("Tabla planes creada.")
+                print("Tabla planes creada.")
+
 
 def planes_app():
     "Función para iniciar el programa."
     app = QApplication(sys.argv)
     app.setStyle("fusion")
-    w = plan()
-    w.show()
+    planes = plan()
+    planes.show()
     sys.exit(app.exec_())
+
 
 class plan(QMainWindow):
     def __init__(self):
         """Método constructor, inicializa los argumentos del objeto 'Plan'"""
         QMainWindow.__init__(self)
-        uic.loadUi("ui_planes.ui", self)
+        uic.loadUi("ui_menu.ui", self)
 
         self.consultaModel = QSqlQueryModel(self)
         self.consultaModel.setQuery("select * from planes")
-        self.tableView.setModel(self.consultaModel)
+        self.ConsultaTable.setModel(self.consultaModel)
+        self.ConsultaTable.verticalHeader().setVisible(False)
+        self.ConsultaTable.setSortingEnabled(True)
         """Métodos para la consulta de planes en la base de datos"""
-        
+
         self.borrarModel = QSqlTableModel(self)
         self.borrarModel.setTable("planes")
         self.borrarModel.select()
-        self.tableView_2.setEditTriggers(QTableView.NoEditTriggers)
-        self.tableView_2.setSelectionBehavior(QTableView.SelectRows)
-        self.tableView_2.setModel(self.borrarModel)
+        self.BorrarTable.setEditTriggers(QTableView.NoEditTriggers)
+        self.BorrarTable.setSelectionBehavior(QTableView.SelectRows)
+        self.BorrarTable.setModel(self.borrarModel)
+        self.BorrarTable.verticalHeader().setVisible(False)
         """Métodos para borrar planes en la base de datos."""
 
         self.actualizarModel = QSqlTableModel(self)
         self.actualizarModel.setTable("planes")
         self.actualizarModel.select()
-        self.tableView_3.setModel(self.actualizarModel)
+        self.ActualizarTable.setModel(self.actualizarModel)
+        self.ActualizarTable.verticalHeader().setVisible(False)
         """Métodos para actualizar los planes en la base de datos."""
 
-        self.lineEdit_6.textEdited.connect(self.onLineEdit_6_textEdited)
+        self.ConsultaIntput.textEdited.connect(
+            self.onConsultaIntput_textEdited)
         """Ejecuta el line edit que consulta específicamente un plan"""
 
-        self.tableView_2.clicked.connect(self.onBorrartableView_2_clicked)
+        self.BorrarTable.clicked.connect(self.onDeleteInfo_clicked)
         """Ejecuta la tabla que borrará info de la base de datos"""
-        
-        self.pushButton.clicked.connect(self.onPushButton_clicked)
+
+        self.ConsultaRefresh.clicked.connect(self.onRefresh_clicked)
         # Botón que al clickarse actualiza la información de la tabla de consultas.
-        self.pushButton_2.clicked.connect(self.onPushButton_2_clicked)
+        self.BorrarRefresh.clicked.connect(self.onRefresh_clicked)
         # Botón que al clickarse actualiza la información de la tabla de borrado.
-        self.pushButton_3.clicked.connect(self.onPushButton_3_clicked)
+        self.ActualizarRefresh.clicked.connect(self.onRefresh_clicked)
+        # Botón que al clickarse actualiza la información de la tabla de update.
+        self.Registrar.clicked.connect(self.onRegister_clicked)
         # Botón que al clickarse ingresa un nuevo plan a la bd.
-        self.pushButton_4.clicked.connect(self.onPushButton_4_clicked)
-        # Botón que al clickarse cierra el programa (Provisional)
-        
-    
-    def onPushButton_4_clicked(self):
-        """Función para cerrar la app (provisional."""
-        self.close()
 
-
-    def onBorrartableView_2_clicked(self, index):
+    def onDeleteInfo_clicked(self, index):
         """Función para borrar registros de la db"""
         if (QMessageBox.question(self, "Borrar datos.", "¿Desea borrar este plan?", QMessageBox.Yes | QMessageBox.No)
-        == QMessageBox.Yes):
+                == QMessageBox.Yes):
             row = index.row()
             if self.borrarModel.removeRow(row):
                 self.borrarModel.select()
 
-    def onLineEdit_6_textEdited(self, txt):
+    def onConsultaIntput_textEdited(self, txt):
         """Función para que un lineedit enseñe info específica en la db"""
-        self.consultaModel.setQuery("select * from planes where codigo like '%" + txt + "%'")
+        self.consultaModel.setQuery(
+            "select * from planes where codigo like '%" + txt + "%'")
 
-    def onPushButton_clicked(self):
+    def onRefresh_clicked(self):
         """Función que le da un botón la capacidad de actualizar la info mostrada en la app"""
         self.consultaModel.setQuery("select * from planes")
-    
-    def onPushButton_2_clicked(self):
-        """Función que le da un botón la capacidad de actualizar la info mostrada en la app"""
         self.borrarModel.select()
+        self.actualizarModel.select()
 
-    def onPushButton_3_clicked(self):
+    def onRegister_clicked(self):
         """Función para registrar planes en la bd."""
 
-        codigo = self.lineEdit_2.text()
-        nombre = self.lineEdit_3.text()
-        valor = self.lineEdit_4.text()
-        cantidad = self.lineEdit_5.text()
+        codigo = self.Reg_CodigoInput.text()
+        nombre = self.Reg_NombreInput.text()
+        valor = self.Reg_ValorInput.text()
+        cantidad = self.Reg_CantidadInput.text()
         """Datos que lee la bd"""
 
-        if (codigo == ""):
+        if (codigo == "" or nombre == "" or valor == "" or cantidad == ""):
             QMessageBox.critical(self, "Error", "Rellene todos los espacios.")
             return
+        """
         if (nombre == ""):
             QMessageBox.critical(self, "Error", "Rellene todos los espacios.")
             return
@@ -112,8 +115,9 @@ class plan(QMainWindow):
         if (cantidad == ""):
             QMessageBox.critical(self, "Error", "Rellene todos los espacios.")
             return
+            """
         """Mensajes de error si falta algún dato por ingresar."""
-        
+
         codigo = int(codigo)
         valor = float(valor)
         cantidad = int(cantidad)
@@ -126,15 +130,15 @@ class plan(QMainWindow):
             q.addBindValue(valor)
             q.addBindValue(cantidad)
             if (q.exec()):
-                if (QMessageBox.question(self, "Listo.", "Listo. ¿Desea limpiar campos?", QMessageBox.Yes | QMessageBox.No) 
-                == QMessageBox.Yes):
-                    self.lineEdit_2.clear()
-                    self.lineEdit_3.clear()
-                    self.lineEdit_4.clear()
-                    self.lineEdit_5.clear()
+                if (QMessageBox.question(self, "Listo.", "Listo. ¿Desea limpiar campos?", QMessageBox.Yes | QMessageBox.No)
+                        == QMessageBox.Yes):
+                    self.Reg_CodigoInput.clear()
+                    self.Reg_NombreInput.clear()
+                    self.Reg_ValorInput.clear()
+                    self.Reg_CantidadInput.clear()
                     """Funciones para limpiar el tablero de ingresar datos."""
 
 
-# if __name__ == "__main__":
-#    ConBD()
-#    planes_app()
+if __name__ == "__main__":
+    ConBD()
+    planes_app()
